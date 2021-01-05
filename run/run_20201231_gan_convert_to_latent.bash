431f6bc4b7d731e85ca52f1bf81638b31c4be17e
Tsuyoshi Inoue <inoue0406@gmail.com>
2021-01-05 18:43:57 +0900

    test cases

--- run/run_20201231_gan_convert_to_latent.bash
+++ run/run_20201231_gan_convert_to_latent.bash
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-#export CUDA_VISIBLE_DEVICES=0
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
-case="result_20201229_gan_convert_to_latent_kanto_valid"
+case="result_20201231_gan_convert_to_latent"
 
 # running script for Rainfall Prediction with ConvLSTM
 python ../src/main_GAN_convert_to_latent_jma.py \
        --dataset radarJMA --data_scaling linear\
-       --data_path ../data/data_kanto/ --image_size 200\
-       --train_path ../data/valid_simple_JMARadar_tmp_small.csv \
+       --data_path ../data/data_alljapan/ --image_size 200\
+       --train_path ../data/train_alljapan_2yrs_JMARadar_tmp_last.csv \
        --result_path $case --tdim_use 12 --learning_rate 0.1 --lr_decay 0.9\
        --batch_size 10 --n_epochs 20 --n_threads 4 \
        --loss_function MSE\
        --optimizer adam \
        --gan_path ../run/result_GAN_201214_testrun_alljapan/model_150.pt\
 
-#       --train_path ../data/valid_simple_JMARadar.csv \
